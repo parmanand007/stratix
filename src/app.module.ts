f2ea5f4dfbd6e235cc99506635ebafb53a9ff839
parmanand.prajapati <parmanand.prajapati@doctustech.com>
2024-09-14 01:19:47 +0530

    fix(ca-cert): add ca cert

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,31 +1,3 @@
-// // src/app.module.ts
-// import { Module, MiddlewareConsumer, NestModule } from '@nestjs/common';
-// import { TypeOrmModule } from '@nestjs/typeorm';
-// import { ConfigModule, ConfigService } from '@nestjs/config';
-// import { dataSourceOptions } from './config/database.config';
-// import { OrganizationModule } from './organization/organization.module';
-// import { LoggerMiddleware } from './middleware/logger.middleware';
-
-// @Module({
-//   imports: [
-//     ConfigModule.forRoot({ isGlobal: true }),  // Load environment variables globally
-//     TypeOrmModule.forRootAsync({
-//       imports: [ConfigModule],
-//       inject: [ConfigService],
-//       useFactory: dataSourceOptions,
-//     }),
-//     OrganizationModule,
-//   ],
-// })
-// export class AppModule implements NestModule {
-//   configure(consumer: MiddlewareConsumer) {
-//     consumer
-//       .apply(LoggerMiddleware)  // Apply the middleware
-//       .forRoutes('*');  // Apply to all routes
-//   }
-// }
-
-
 
 import { Module, MiddlewareConsumer, NestModule } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,20 +1,43 @@
 import { ConfigService } from '@nestjs/config';
 import { DataSourceOptions, DataSource } from 'typeorm';
+import * as fs from 'fs';
+import * as path from 'path';
 
-export const dataSourceOptionsFactory = (configService: ConfigService): DataSourceOptions => ({
-  type: 'postgres',
-  host: configService.get<string>('DB_HOST'),
-  port: parseInt(configService.get<string>('DB_PORT'), 10) ?? 5432,
-  username: configService.get<string>('DB_USERNAME'),
-  password: configService.get<string>('DB_PASSWORD'),
-  database: configService.get<string>('DB_NAME'),
-  synchronize: false,
-  // logging: true,
-  entities: [__dirname + '/../**/*.entity{.ts,.js}'],  // Adjust the path if needed
-  migrations: [__dirname + '/../migrations/*{.ts,.js}'],  // Adjust the path if needed
-  migrationsRun: true,
-});
+export const dataSourceOptionsFactory = (configService: ConfigService): DataSourceOptions => {
+  // Read SSL certificates paths from ConfigService
+  const sslCaPath = configService.get<string>('SSL_CA_CERTIFICATES');
+  
 
-const dataSource = new DataSource(dataSourceOptionsFactory(new ConfigService()));
+  const ssl = sslCaPath  ? {
+    ca: fs.readFileSync(path.resolve(sslCaPath)),
+    
+  } : undefined;
+
+   // Log environment variables for debugging purposes
+   console.log('DB_HOST:', configService.get<string>('DB_HOST'));
+   console.log('DB_PORT:', configService.get<string>('DB_PORT'));
+   console.log('DB_USERNAME:', configService.get<string>('DB_USERNAME'));
+   console.log('DB_PASSWORD:', configService.get<string>('DB_PASSWORD'));
+   console.log('DB_NAME:', configService.get<string>('DB_NAME'));
+
+  return {
+    type: 'postgres',
+    host: configService.get<string>('DB_HOST') ,
+    port: parseInt(configService.get<string>('DB_PORT'), 10) ?? 5432,
+    username: configService.get<string>('DB_USERNAME') ,
+    password: configService.get<string>('DB_PASSWORD') ,
+    database: configService.get<string>('DB_NAME') ,
+    synchronize: false,
+    // logging: true,
+    entities: [__dirname + '/../**/*.entity{.ts,.js}'],  // Adjust the path if needed
+    migrations: [__dirname + '/../migrations/production/*{.ts,.js}'],  // Adjust the path if needed
+    // migrations: ['./migrations/*{.ts,.js}','./migrations/production/*{.ts,.js}'],  // Adjust the path if needed
+    migrationsRun: true,
+    ssl,  // Add the SSL configuration
+  };
+};
+
+const configService = new ConfigService();
+const dataSource = new DataSource(dataSourceOptionsFactory(configService));
 
 export default dataSource;
