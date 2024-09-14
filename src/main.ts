1010e045a20596be466941e2e2ceff37b3becb18
parmanand.prajapati <parmanand.prajapati@doctustech.com>
2024-09-14 11:35:42 +0530

    fix(swagger-setup): add custom js and custom css

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,13 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('/api', app, document, {
+    customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js',
+    ],
+  });
   
 
   await app.listen(3000);
